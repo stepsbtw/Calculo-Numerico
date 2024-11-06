87ff3473931804ff2c716348adf8f867667aedcd
Caio Passos Torkst Ferreira <stepsbtw@gmail.com>
2024-11-06 11:46:56 -0300

    Update solve_upper.m

--- solve_upper.m
+++ solve_upper.m
@@ -2,12 +2,12 @@
 function x = solve_upper (U, b)
   n = length(b); x = zeros(n,1);
 
-  for i= n:-1:1
-    x(i) = b(i) / U(i,i);
-    for j = i:-1:1
-      x(j) = (b(j) - U(i,j) * x(i)) / U(i,i)
-
+  for i= n:-1:1 # from last line to first
+    x(i) = b(i);
+    for j = i+1:-1:n # from next column
+      x(j) = x(i) - U(i,j) * x(j);
     endfor
+    x(i) = x(i) / U(i,i) # divide by "pivot"
   endfor
 
 endfunction
