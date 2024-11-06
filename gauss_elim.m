740b0bb0df15d6d03e7ca026fb45b4842235b0d7
Caio Passos Torkst Ferreira <stepsbtw@gmail.com>
2024-11-06 11:39:09 -0300

    fixed indices

--- gauss_elim.m
+++ gauss_elim.m
@@ -2,7 +2,7 @@
 function upper = gauss_elim (A)
   n = size(A,1);
 
-  for j = 1:n # every column
+  for j = 1:n-1 # every column but not last one
     for i = i+1:n # pivot from next line
       M = A(i,j)/A(j,j);
       for k = j:n # every element in line
